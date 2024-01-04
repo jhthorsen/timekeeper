236c1cf78853c93610b20857fe22762a30fb3ec0
Jan Henning Thorsen <jan.henning@thorsen.pm>
2024-01-04 10:00:34 +0900

    Stop depending on webpack

--- assets/index.js
+++
@@ -1,9 +0,0 @@
-import './sass/timekeeper.scss';
-import Timekeeper from './Timekeeper';
-import {on, q} from './js/util';
-
-const t = new Timekeeper().attach(document.querySelector('form'));
-
-q(document, 'a[href$="#alarm"]', el => on(el, 'click', e => t.toggleAlarm(e)));
-q(document, 'a[href^="#start:"]', el => on(el, 'click', () => t.start(el.href.split('#start:')[1])));
-q(document, 'a[href^="#stop"]', el => on(el, 'click', () => t.stop()));
--- cpanfile
+++ cpanfile
@@ -1,6 +1,5 @@
 # You can install this project with curl -L http://cpanmin.us | perl - https://github.com/jhthorsen/app-timekeeper/archive/master.tar.gz
-requires "Mojolicious"                  => "8.00";
-requires "Mojolicious::Plugin::Webpack" => "0.05";
-requires "Time::Duration"               => "1.00";
+requires "Mojolicious"    => "9.00";
+requires "Time::Duration" => "1.00";
 
 test_requires "Test::More" => "0.88";
